787ae028c9fd4aada05908cea547aa36d19feac7
1224774-sketch <1224774@student.roc-nijmegen.nl>
2026-09-16 11:04:24 +0200

    initial commit

---
+++ src/basisProgrammeren1/Variables03.java
@@ -0,0 +1,20 @@
+package basisProgrammeren1;
+public class Variables03 {
+
+    public static void main(String[] args) {
+        
+    	String g = "Green";
+    	String y = "Yellow";
+    	String r = "Red";
+    	String p = "Purple";
+    	String b = "Blue";
+    	String o = "Orange";
+    	
+    	//DINGETJES
+    	String g = "green";
+    	String g = "green";
+    	String g = "green";
+    	
+    	
+    }
+}
